3047fd9a61f04b1e546d849e81631dca9a075a70
Chris Nady <nady.christopher@gmail.com>
2017-08-21 22:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
      return "I can't hear you!"
    } else if (upperCase === string) {
      return "YES INDEED!"
-   } else if (string === "I love you, Grandma") {
+   } else if (string === "I love you, Grandma.") {
      return "I love you, too."
    }
 }
